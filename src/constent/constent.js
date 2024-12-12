064a7d676030fcf9fa636e701f2eca67287788a5
Skulldued <skulldued30@gmail.com>
2024-12-12 23:33:51 +0530

    first commit

---
+++ src/constent/constent.js
@@ -0,0 +1,55 @@
+import about1 from "../assets/about-1.png";
+import about2 from "../assets/about-2.png";
+import about3 from "../assets/about-3.png";
+import ai from "../assets/ai.png";
+import arrowMove from "../assets/arrow-move.png";
+import boat1 from "../assets/boat-1.png";
+import boat2 from "../assets/boat-2.png";
+import footerBg from "../assets/footer-bg.png";
+import heroBanner from "../assets/hero-banner.png";
+import logoNav from "../assets/logo3.png";
+import news1 from "../assets/news-1.png";
+import news2 from "../assets/news-2.png";
+import news3 from "../assets/news-3.png";
+import newsSm from "../assets/news-sm.png";
+import result1 from "../assets/result-1.png";
+import result2 from "../assets/result-2.png";
+import result3 from "../assets/result-3.png";
+import result4 from "../assets/result-4.png";
+import robotLeft from "../assets/robots-left.png";
+import square from "../assets/square.png";
+import squareDot from "../assets/square-dot.png";
+import team1 from "../assets/team-1.png";
+import team2 from "../assets/team-2.png";
+import team3 from "../assets/team-3.png";
+import testimonialBg from "../assets/testimonial-bg.png";
+import bgTech from "../assets/bg-tech.png";
+
+export const assets = {
+  bgTech,
+  testimonialBg,
+  team3,
+  team2,
+  team1,
+  squareDot,
+  square,
+  robotLeft,
+  result4,
+  result3,
+  result2,
+  result1,
+  newsSm,
+  news3,
+  news2,
+  news1,
+  logoNav,
+  heroBanner,
+  footerBg,
+  boat2,
+  boat1,
+  arrowMove,
+  ai,
+  about3,
+  about2,
+  about1,
+};
