83c00ea9efc6382138a9680a8435387f25d9c734
Skulldued <skulldued30@gmail.com>
2024-12-13 23:41:27 +0530

    About us section completed according to desktop view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
-
+import React from "react";
+import { assets } from "../constent/constent";
+import { IoMdPlay } from "react-icons/io";
 const Header = () => {
   return (
-    <div>
-      
+    <div className="w-full bg-home-header bg-cover  md:-mt-[190px] relative ">
+      <div className="absolute bottom-72  lg:top-64  md:right-1/3 ">
+        <img src={assets.arrowMove} className="animate-animate-x" alt="" />
+      </div>
+      <div className="absolute bottom-20 md:right-[750px] ">
+        <img src={assets.square} className="animate-animate-y md:w-48" alt="" />
+      </div>
+      <div className="px-5 lg:px-40">
+        <div className="lg:flex  items-center pt-28 md:pt-72 ">
+          <div className="lg:w-2/5">
+            <div className=" my-5 md:mb-3">
+              <span className="py-2 px-4  text-white text-xl rounded-full bg-light-green ">
+                Designing for the future
+              </span>
+            </div>
+            <div>
+              <h1 className="lg:text-[65px] text-[40px] leading-snug text-white font-bold font-lora-font">
+                Empowering Your Business With Artificial
+              </h1>
+              <p className="my-2 text-slate-400 text-xl font-inter">
+                And In Order To Make A Business, Brand Advertising And Marketing
+                Plays An Important Role. Similarly,
+              </p>
+              <div className="flex gap-10 md:my-10   items-center">
+                <div>
+                  <button className="md:py-3 py-2 font-inter border-text-website px-3 md:px-5  md:text-2xl  border-2 rounded-full border-c text-white ">
+                    Discover More +
+                  </button>
+                </div>
+                <div>
+                  <div className="w-12 h-12 bg-white rounded-full flex items-center justify-center">
+                    <IoMdPlay className="text-3xl text-blue-700" />
+                  </div>
+                </div>
+                <div className="text-white text-xl md:text-2xl font-inter">
+                  how we work
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-end lg:w-3/5 pt-32 md:pt-20 lg:pt-0">
+            <div>
+              <img
+                src={assets.robotLeft}
+                className=" w-52 md:w-72 lg:w-[550px]"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,55 +1,68 @@
 import React from "react";
+import Header from "../components/Header";
+import { RiCheckboxCircleFill } from "react-icons/ri";
 import { assets } from "../constent/constent";
-import { IoMdPlay } from "react-icons/io";
 const Home = () => {
   return (
-    <div className="w-full bg-home-header bg-cover  md:-mt-[190px] ">
-      <div className="absolute bottom-72  lg:top-64  md:right-1/3 ">
-        <img src={assets.arrowMove} className="animate-animate-x" alt="" />
-      </div>
-      <div className="absolute bottom-20 md:right-[750px] ">
-        <img src={assets.square} className="animate-animate-y md:w-48" alt="" />
-      </div>
-      <div className="px-5 lg:px-40">
-        <div className="lg:flex  items-center pt-28 md:pt-72 ">
-          <div className="lg:w-2/5">
-            <div className=" my-5 md:mb-3">
-              <span className="py-2 px-4  text-white text-xl rounded-full bg-light-green ">
-                Designing for the future
-              </span>
-            </div>
+    <div className="w-full">
+      <Header />
+      <div className="container  mx-auto md:pt-40 relative">
+        <div className="grid md:grid-cols-2 grid-cols-1 ">
+          <div className="flex items-center relative">
             <div>
-              <h1 className="lg:text-[65px] text-[40px] leading-snug text-white font-bold font-lora-font">
-                Empowering Your Business With Artificial
+              <img src={assets.about1} className="absolute top-0 animate-animate-y" alt="" />
+            </div>
+            <div >
+              <img src={assets.about2} className="w-5/6" alt="" />
+            </div>
+          </div>
+          <div>
+            <div>
+              <h4 className="text-text-website  md:text-xl my-4">
+                ABOUT COMPANY
+              </h4>
+              <h1 className="md:text-6xl font-lora-font font-bold text-2xl">
+                Experience intelligence like never before
               </h1>
-              <p className="my-2 text-slate-400 text-xl font-inter">
-                And In Order To Make A Business, Brand Advertising And Marketing
-                Plays An Important Role. Similarly,
-              </p>
-              <div className="flex gap-10 md:my-10   items-center">
-                <div>
-                  <button className="md:py-3 py-2 font-inter border-text-website px-3 md:px-5  md:text-2xl  border-2 rounded-full border-c text-white ">
-                    Discover More +
-                  </button>
+              <div>
+                <p className="py-8 font-inter text-xl text-slate-700 text-justify">
+                  You can access SaaS applications through a web browser or
+                  mobile app, as long as you have an internet connection.
+                </p>
+              </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2">
+              <div>
+                <div className="flex items-center">
+                  <RiCheckboxCircleFill className="text-text-website mr-2 md:mr-5 text-xl md:text-3xl" />
+                  <span className="text-xl font-inter">
+                    Mistakes To Avoid to the
+                  </span>
                 </div>
-                <div>
-                  <div className="w-12 h-12 bg-white rounded-full flex items-center justify-center">
-                    <IoMdPlay className="text-3xl text-blue-700" />
-                  </div>
+                <div className="flex items-center my-2 md:my-4">
+                  <RiCheckboxCircleFill className="text-text-website mr-2 md:mr-5 text-xl md:text-3xl" />
+                  <span className="text-xl font-inter">
+                    Your Startup industry stan
+                  </span>
                 </div>
-                <div className="text-white text-xl md:text-2xl font-inter">
-                  how we work
+              </div>
+              <div>
+                <div className="flex items-center">
+                  <RiCheckboxCircleFill className="text-text-website mr-2 md:mr-5 text-xl md:text-3xl" />
+                  <span className="text-xl font-inter">
+                    Mistakes To Avoid to the
+                  </span>
+                </div>
+                <div className="flex items-center my-2 md:my-4">
+                  <RiCheckboxCircleFill className="text-text-website mr-2 md:mr-5 text-xl md:text-3xl" />
+                  <span className="text-xl font-inter">
+                    Your Startup industry stan
+                  </span>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="flex justify-end lg:w-3/5 pt-32 md:pt-20 lg:pt-0">
-            <div>
-              <img
-                src={assets.robotLeft}
-                className=" w-52 md:w-72 lg:w-[550px]"
-                alt=""
-              />
+            <div className="my-4 md:my-8">
+              <button className="bg-text-website rounded-md px-2 md:px-8 py-2 md:py-4 text-white text-xl ">Discover More + </button>
             </div>
           </div>
         </div>
